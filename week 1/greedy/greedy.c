1990ca7292c346e034287ba2a77102f406f5630e
XDer <9876543210deniss@gmail.com>
2018-12-10 10:42:57 +0200

    Update greedy.c

--- week 1/greedy/greedy.c
+++ week 1/greedy/greedy.c
@@ -3,29 +3,27 @@
 
 int main(void)
 {
-    int n, c, t;
+    int n, count;
+
+    // Asks the user how much change is owed.
     do
     {
         n = get_float("Enter dollar amount:") * 100;
     }
     while (n < 0);
 
-    c = 0;
-    // Amount of 25 cents
-    t = (n / 25);
-    c += t;
-    n -= 25 * c;
-    // Amount of 10 cents
-    t = (n / 10);
-    c += t;
-    n -= 10 * t;
-    // Amount of 5 cents
-    t = (n / 5);
-    c += t;
-    n -= 5 * t;
-    // Amount of 1 cents
-    c += n;
+    // Count the minimum number of coins.
+    count = n / 25;
+    n %= 25;
 
-    // Check the result
-    printf("%i\n", c);
+    count += n / 10;
+    n %= 10;
+
+    count += n / 5;
+    n %= 5;
+
+    count += n;
+
+    // Show the result.
+    printf("%i\n", count);
 }
